60f11c64bb7f2c42bc0edb17ab95ad4968c8e494
Maxim Klochkov <maxim.klochkov@gmail.com>
2023-01-03 21:18:04 +0300

    Initial clean setup and commit

---
+++ jsx/src/index.js
@@ -0,0 +1,9 @@
+// 1) Import React and ReactDOM libraries
+
+// 2) Get a refernce to the DIV with ID of "root"
+
+// 3) Tell React to take control of that element
+
+// 4) Create a component
+
+// 5) Show the component on the screen
