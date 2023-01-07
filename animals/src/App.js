34cdd9e164521a30206e1e289c5cdf88cf966cea
Maxim Klochkov <maxim.klochkov@gmail.com>
2023-01-08 01:46:19 +0300

    Add basic useState

--- animals/src/App.js
+++ animals/src/App.js
@@ -1,8 +1,17 @@
+import { useState } from 'react'
+
 import AnimalShow from "./AnimalShow"
 
 function App() {
+    const [count, setCount] = useState(0)
+
+    const handleClick = () => {
+        setCount(count + 1)
+    }
+
     return <div>
-        Show Animal list here
+        <button onClick={handleClick}>Add Animal</button>
+        <div>Number of Animals: { count }</div>
         <AnimalShow />
     </div>
 }
