15d7ac1ec58c8a1184b1c5d79c38ed9d7b32850a
Maxim Klochkov <maxim.klochkov@gmail.com>
2023-01-10 00:57:56 +0300

    Add Heart icon and image click handler

--- animals/src/AnimalShow.js
+++ animals/src/AnimalShow.js
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 import bird from './svg/bird.svg'
 import cat from './svg/cat.svg'
 import cow from './svg/cow.svg'
@@ -16,9 +18,22 @@ const svgMap = {
 }
 
 function AnimalShow({ type }) {
-    return <div>
-        <img src={svgMap[type]} alt="animal" />
-    </div>
+    const [clicks, setClicks] = useState(0)
+
+    const handleClick = () => {
+        setClicks(clicks + 1)
+    }
+
+    return (
+        <div onClick={handleClick}>
+            <img src={svgMap[type]} alt="animal" />
+            <img
+                src={heart}
+                alt="heart"
+                style={{ width: 10 + 10 * clicks + 'px' }}
+            />
+        </div>
+    )
 }
 
 export default AnimalShow
